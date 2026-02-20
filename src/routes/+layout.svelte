8b837e040f67b6200a96b220c1a787bfda291b82
Samuel Åkesson <samak519@student.liu.se>
2026-02-20 20:29:23 +0100

    minor changs

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import yalc_logo from '$lib/assets/yalc-logo.svg';
+	import lisam_logo from '$lib/assets/lisam-logo.png';
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="icon" href={yalc_logo} />
+</svelte:head>
+
 {@render children()}
