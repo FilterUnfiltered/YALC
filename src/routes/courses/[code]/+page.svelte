339e699a0e735fed80a926f4e175e594683e5029
Samuel Åkesson <samak519@student.liu.se>
2026-02-21 14:10:27 +0100

    start styling course logo. Add some description

--- src/routes/courses/[code]/+page.svelte
+++ src/routes/courses/[code]/+page.svelte
@@ -1,6 +1,22 @@
 <script lang="ts">
   let { data } = $props();
   const course = data.course;
+
+  const first_two_letters_of_course_name = course.name.slice(0, 2).toUpperCase();
+  // random dark color based on `first_two_letters_of_course_name`
+  const logoColor = `hsl(${first_two_letters_of_course_name.charCodeAt(0) * 10}, 70%, 50%)`;
 </script>
 
-<h1>{course.code} – {course.name}</h1>
+<h1 class="flex items-center gap-2 text-2xl font-semibold">
+  <div
+    class="flex h-12 w-12 items-center justify-center"
+    style="background-color: {logoColor}"
+  >
+    {first_two_letters_of_course_name}
+  </div>
+  {course.code} – {course.name}
+</h1>
+
+<div class="flex">
+  <div></div>
+</div>
