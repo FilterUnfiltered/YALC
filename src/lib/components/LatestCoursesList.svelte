58f60e5f2919e6c68ef0e1b5bc1b46436765859d
Samuel Åkesson <samak519@student.liu.se>
2026-02-21 11:31:53 +0100

    style courselist

--- src/lib/components/LatestCoursesList.svelte
+++ src/lib/components/LatestCoursesList.svelte
@@ -1,16 +1,19 @@
 <script lang="ts">
-  import yaml from 'js-yaml';
-
-  import coursesYamlString from '$lib/assets/courses.yaml?raw';
-  type Course = {
+  type Course = {
     code: string;
     name: string;
   };
+  import yaml from 'js-yaml';
+  import coursesYamlString from '$lib/assets/courses.yaml?raw';
   const courses = yaml.load(coursesYamlString) as Course[];
+
+  import { GraduationCap } from '@lucide/svelte';
 </script>
 
-<div class="grid">
+<div class="grid grid-cols-2 items-center">
   {#each courses as course }
-    <div>{course.name}</div> 
+    <!-- FIXME: use type-safe/corrected href? -->
+    <a class="whitespace-nowrap truncate" href={course.code}>{course.code} - {course.name}</a> 
+    <GraduationCap />
   {/each}
 </div>
