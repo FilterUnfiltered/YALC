b9f4906392fcd976959dbac39bc2124f72457d3c
Samuel Åkesson <samak519@student.liu.se>
2026-02-21 11:12:35 +0100

    begin creating LatestCourseList (#6)

---
+++ src/lib/components/LatestCoursesList.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import yaml from 'js-yaml';
+
+  import coursesYamlString from '$lib/assets/courses.yaml?raw';
+  type Course = {
+    code: string;
+    name: string;
+  };
+  const courses = yaml.load(coursesYamlString) as Course[];
+</script>
+
+<div class="grid">
+  {#each courses as course }
+    <div>{course.name}</div> 
+  {/each}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,10 @@
 <script lang="ts">
   import academic_hat from '$lib/assets/icons/academic_hat.svg';
   import Heading from '$lib/components/Heading.svelte';
+  import LatestCoursesList from '$lib/components/LatestCoursesList.svelte';
 </script>
 
 <Heading>Courses and programs</Heading>
-<div class="grid">
-  <a href="#">
-    6CDDD - Computer Science and Engineering, M Sc in Engineering
-    <img src={academic_hat} />
-  </a>
-  <a href="#">6CDDD - Computer Science and Engineering, M Sc in Engineering</a>
-  <a href="#">6CDDD - Computer Science and Engineering, M Sc in Engineering</a>
-  <a href="#">6CDDD - Computer Science and Engineering, M Sc in Engineering</a>
-</div>
+<LatestCoursesList />
 
 <h2>Upcoming for you</h2>
