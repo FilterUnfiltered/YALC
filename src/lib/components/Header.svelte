ee1c73ca576bf8770ce49a815824b9fd46e50825
Ermia Behzadifar <ermbe305@student.liu.se>
2026-02-21 10:48:06 +0100

    move header to component

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import liu_notext_black from '$lib/assets/LiU-Black_Liu_noText.svg';
+
+  let { children } = $props();
+</script>
+
+<header>
+  <nav class="flex items-center gap-4 bg-[#80dcf3]">
+    <button class="p-4 hover:bg-gray-300" aria-label="menu-select">
+      <svg
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+      >
+        <circle cx="5" cy="5" r="1" fill="currentColor" />
+        <circle cx="12" cy="5" r="1" fill="currentColor" />
+        <circle cx="19" cy="5" r="1" fill="currentColor" />
+        <circle cx="5" cy="12" r="1" fill="currentColor" />
+        <circle cx="12" cy="12" r="1" fill="currentColor" />
+        <circle cx="19" cy="12" r="1" fill="currentColor" />
+        <circle cx="5" cy="19" r="1" fill="currentColor" />
+        <circle cx="12" cy="19" r="1" fill="currentColor" />
+        <circle cx="19" cy="19" r="1" fill="currentColor" />
+      </svg>
+    </button>
+    <img class="mr-20" width="30" src={liu_notext_black} alt="YALC logo" />
+    <b class="mr-20">Share</b><b>Point</b>
+  </nav>
+</header>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,7 @@
   import liu_primar_vit from '$lib/assets/LiU_primar_vit.svg';
   import liu_notext_black from '$lib/assets/LiU-Black_Liu_noText.svg';
   import Sidebar from '$lib/components/Sidebar.svelte';
+  import Header from '$lib/components/Header.svelte';
 
   let { children } = $props();
 </script>
@@ -13,32 +14,7 @@
   <link rel="icon" href={yalc_logo} />
 </svelte:head>
 
-<header>
-  <nav class="flex items-center gap-4 bg-[#80dcf3]">
-    <button class="p-4 hover:bg-gray-300" aria-label="menu-select">
-      <svg
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        stroke-width="2"
-      >
-        <circle cx="5" cy="5" r="1" fill="currentColor" />
-        <circle cx="12" cy="5" r="1" fill="currentColor" />
-        <circle cx="19" cy="5" r="1" fill="currentColor" />
-        <circle cx="5" cy="12" r="1" fill="currentColor" />
-        <circle cx="12" cy="12" r="1" fill="currentColor" />
-        <circle cx="19" cy="12" r="1" fill="currentColor" />
-        <circle cx="5" cy="19" r="1" fill="currentColor" />
-        <circle cx="12" cy="19" r="1" fill="currentColor" />
-        <circle cx="19" cy="19" r="1" fill="currentColor" />
-      </svg>
-    </button>
-    <img class="mr-20" width="30" src={liu_notext_black} alt="YALC logo" />
-    <b class="mr-20">Share</b><b>Point</b>
-  </nav>
-</header>
+<Header />
 
 <Sidebar />
 
