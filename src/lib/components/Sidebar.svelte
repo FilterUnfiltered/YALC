b215abc89c652119efe85bd57cd5346379e2ddfa
Ermia Behzadifar <ermbe305@student.liu.se>
2026-02-21 16:00:24 +0100

    fix hover

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -8,49 +8,49 @@
 
 <ul class="flex min-h-full w-14 flex-col items-center gap-4 bg-gray-100 dark:bg-gray-900">
   <li
-    class="flex h-12 w-14 items-center justify-center hover:cursor-pointer hover:bg-gray-500 dark:hover:bg-gray-700"
+    class="flex h-12 w-14 items-center justify-center hover:cursor-pointer hover:bg-gray-200 dark:hover:bg-gray-700"
   >
     <a href={resolve('/')}>
       <img src={theme.light ? liu_logo : light_liu_logo} alt="LiU logo" class="h-6 w-6" />
     </a>
   </li>
   <li
-    class="flex h-12 w-14 items-center justify-center hover:cursor-pointer hover:bg-gray-500 dark:hover:bg-gray-700"
+    class="flex h-12 w-14 items-center justify-center hover:cursor-pointer hover:bg-gray-200 dark:hover:bg-gray-700"
   >
     <a href={resolve('/courses')}>
       <Globe class="h-6 w-6 text-gray-500 dark:text-gray-300" />
     </a>
   </li>
   <li
-    class="flex h-12 w-14 items-center justify-center hover:cursor-pointer hover:bg-gray-500 dark:hover:bg-gray-700"
+    class="flex h-12 w-14 items-center justify-center hover:cursor-pointer hover:bg-gray-200 dark:hover:bg-gray-700"
   >
     <a href={resolve('/news')}>
       <Newspaper class="h-6 w-6 text-gray-500 dark:text-gray-300" />
     </a>
   </li>
   <li
-    class="flex h-12 w-14 items-center justify-center hover:cursor-pointer hover:bg-gray-500 dark:hover:bg-gray-700"
+    class="flex h-12 w-14 items-center justify-center hover:cursor-pointer hover:bg-gray-200 dark:hover:bg-gray-700"
   >
     <a href={resolve('/files')}>
       <File class="h-6 w-6 text-gray-500 dark:text-gray-300" />
     </a>
   </li>
   <li
-    class="flex h-12 w-14 items-center justify-center hover:cursor-pointer hover:bg-gray-500 dark:hover:bg-gray-700"
+    class="flex h-12 w-14 items-center justify-center hover:cursor-pointer hover:bg-gray-200 dark:hover:bg-gray-700"
   >
     <a href={resolve('/lists')}>
       <TableProperties class="h-6 w-6 text-gray-500 dark:text-gray-300" />
     </a>
   </li>
   <li
-    class="flex h-12 w-14 items-center justify-center hover:cursor-pointer hover:bg-gray-500 dark:hover:bg-gray-700"
+    class="flex h-12 w-14 items-center justify-center hover:cursor-pointer hover:bg-gray-200 dark:hover:bg-gray-700"
   >
     <a href={resolve('/connections')}>
       <Puzzle class="h-6 w-6 text-gray-500 dark:text-gray-300" />
     </a>
   </li>
   <li
-    class="flex h-12 w-14 items-center justify-center hover:cursor-pointer hover:bg-gray-500 dark:hover:bg-gray-700"
+    class="flex h-12 w-14 items-center justify-center hover:cursor-pointer hover:bg-gray-200 dark:hover:bg-gray-700"
   >
     <a href={resolve('/create')}>
       <CirclePlus class="h-6 w-6 text-gray-500 dark:text-gray-300" />
