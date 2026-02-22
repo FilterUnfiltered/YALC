3559b90a5c32fc16dfc45af09bdf80060b7d5bdb
B1n-ry <b1nry.dev@gmail.com>
2026-02-22 10:30:55 +0100

    Fix upcoming for you

--- src/lib/components/UpcomingForYou.svelte
+++ src/lib/components/UpcomingForYou.svelte
@@ -2,31 +2,31 @@
   import DifferingSpeedLink from './DifferingSpeedLink.svelte';
 </script>
 
-<div
-  class="inline-grid w-full grid-cols-[1fr_50px] items-stretch border-b border-[#03787c] dark:bg-[#03787c]"
->
-  <DifferingSpeedLink
-    class="truncate border-t border-[#03787c] p-4 text-lg font-semibold whitespace-nowrap"
-    to=""
-  >
-    Death to all students - TATB03
-  </DifferingSpeedLink>
-  <svg
-    width="2em"
-    height="2em"
-    viewBox="0 0 32 32"
-    class="box-content border-t border-[#03787c] p-4 text-[#03787c]"
-    ><title>Student</title><g
-      stroke-width="1.5"
-      fill="none"
-      fill-rule="evenodd"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      ><path
-        d="M24.5 14.614v4.957C24.5 21.465 20.694 23 16 23s-8.5-1.535-8.5-3.43v-4.956l8.5 3.391 8.5-3.391zM2 12.418L16 18l14-5.582L16 8 2 12.418z"
-        stroke="#FFF"
-        fill="currentColor"
-      ></path><path stroke="currentColor" d="M16 12l11 .962V22"></path></g
-    ></svg
-  >
-</div>
+<ul class="w-full items-stretch border-b border-[#03787c] dark:bg-[#03787c]">
+  <li class="flex w-full flex-row">
+    <DifferingSpeedLink
+      class="truncate border-t border-[#03787c] p-4 text-lg font-semibold whitespace-nowrap"
+      to=""
+    >
+      Death to all students - TATB03
+    </DifferingSpeedLink>
+    <svg
+      width="2em"
+      height="2em"
+      viewBox="0 0 32 32"
+      class="box-content border-t border-[#03787c] p-4 text-[#03787c]"
+      ><title>Student</title><g
+        stroke-width="1.5"
+        fill="none"
+        fill-rule="evenodd"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        ><path
+          d="M24.5 14.614v4.957C24.5 21.465 20.694 23 16 23s-8.5-1.535-8.5-3.43v-4.956l8.5 3.391 8.5-3.391zM2 12.418L16 18l14-5.582L16 8 2 12.418z"
+          stroke="#FFF"
+          fill="currentColor"
+        ></path><path stroke="currentColor" d="M16 12l11 .962V22"></path></g
+      ></svg
+    >
+  </li>
+</ul>
